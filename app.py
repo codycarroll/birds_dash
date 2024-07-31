be3eae169d34fde5e2e850749d15eebf0a5b6a5e
Cody Carroll <cjcarroll@usfca.edu>
2024-07-31 14:56:51 -0700

    fix app add comma

--- app.py
+++ app.py
@@ -294,7 +294,7 @@ def update_comparison_plot(bird_ids, plot_type, data_type, scale_type, selected_
             fig = circular_plot(bird_dict, birds_df, [bird_id], scale_type, max_y_value)
         
         plots.append(dcc.Graph(figure=fig, 
-                               config={"toImageButtonOptions": {"width": 750, "height": 600, "scale": 2, "filename": 'bird_szn_figure2'}}
+                               config={"toImageButtonOptions": {"width": 750, "height": 600, "scale": 2, "filename": 'bird_szn_figure2'}},
                                style={'display': 'inline-block', 'width': '45%', 'margin': '10px', 'border': '1px solid lightgrey'}))
 
     return html.Div(plots, style={'textAlign': 'center'})
