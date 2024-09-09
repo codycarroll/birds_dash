fa5c2e6ac6efa0ce87ca537446c35a08e8a10c75
Cody Carroll <cjcarroll@usfca.edu>
2024-09-08 21:22:07 -0700

    adj line breaks

--- app.py
+++ app.py
@@ -229,11 +229,13 @@ app.layout = html.Div([
     ]),
 
     html.Br(),  # Line Break
-    html.Br(),
     html.Div(children='''
     To cite this app, use the following citation:
     C. Carroll and S. Waterman. "NorCal Bird Dashboard." 2024. https://birds-dash-547zxcr6ea-uc.a.run.app/
     '''),
+
+    html.Br(),  # Line Break
+    html.Br(),
 
     html.Div([
         html.H2('Bird Seasonality Pattern', style={'textAlign': 'left', 'fontWeight': 'bold'}),
